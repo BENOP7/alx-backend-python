1cb82fa92bd35deb01df057e6079426085f0be23
benop7 <benjaminpetengong@mail.com>
2024-11-07 07:50:29 +0000

    fix pycodestyle errors

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+'''
+    python module
+'''
+
 
 from typing import List, Tuple, Sequence, Iterable
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+        count the elements of the iterable
+    '''
     return [(i, len(i)) for i in lst]
